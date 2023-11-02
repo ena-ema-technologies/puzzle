3eb5f4b8e4526c914f947a2aba08db73e3074d68
Saiful Islam <sakibbhaiya143@gmail.com>
2023-11-02 12:26:24 +0600

    Update

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const hamburger = document.querySelector(".hamburger");
 const phoneLinks = document.querySelector(".phone-links");
+const testimonials = document.querySelectorAll('.testimonial');
+const dots = document.querySelectorAll('.navigation-dot');
+const dotContainer = document.querySelector('.dot-container');
 
 hamburger.addEventListener("click" , ()=>{
     hamburger.classList.toggle("active");
     phoneLinks.classList.toggle("phone-links-active");
 })
+
+
+let currentIndex = 0;
+
+let createDots = ()=> {
+  testimonials.forEach((testimonial, index) => {
+    const dot = document.createElement('span');
+    dot.className = 'navigation-dot';
+    dot.addEventListener('click', () => {
+      currentIndex = index;
+      showTestimonial(currentIndex);
+    });
+    dotContainer.appendChild(dot);
+  });
+}
+
+let showTestimonial = (index)=> {
+  testimonials.forEach((testimonial) => (testimonial.classList.remove('active-testimonial')));
+  const dots = dotContainer.querySelectorAll('.navigation-dot');
+  dots.forEach((dot) => dot.classList.remove('active-dot'));
+
+  testimonials[index].classList.add('active-testimonial');
+  dots[index].classList.add('active-dot');
+}
+
+let nextTestimonial = ()=> {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+createDots();
+showTestimonial(currentIndex);
+setInterval(nextTestimonial, 5000);
+
+
+
+
+
